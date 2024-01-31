3771e014dd183840e2c819f91c38615b86a02382
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-01-31 20:00:25 -0300

    49 to 64

--- Building_an_RPG/script.js
+++ Building_an_RPG/script.js
@@ -16,6 +16,28 @@ const goldText  = document.querySelector("#goldText");
 const monsterStats  = document.querySelector("#monsterStats");
 const monsterName  = document.querySelector("#monsterName");
 const monsterHealthText = document.querySelector("#monsterHealth");
+const locations = [
+    {
+        name: "town square",
+        "button text": ["Go to store", "Go to cave", "Fight dragon"],
+        "button functions": [
+            button1.onclick = goStore,
+            button2.onclick = goCave,
+            button3.onclick = fightDragon
+        ],
+        text: "You are in the town square. You see a sign that says \"Store\"."
+    },
+    {
+        name: "store",
+        "button text": ["Buy 10 health (10 gold)", "Buy weapon (30 gold)", "Go to town square"],
+        "button functions": [
+            button1.onclick = buyHealth,
+            button2.onclick = buyWeapon,
+            button3.onclick = goTown
+        ],
+        text: "You enter the store."
+    }
+  ];
 
 //initialize buttons
 button1.onclick = goStore;
@@ -23,13 +45,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function goTown() {
-    button1.innerText = "Go to store";
-    button2.innerText = "Go to cave";
-    button3.innerText = "Fight dragon";
-    button1.onclick = goStore;
-    button2.onclick = goCave;
-    button3.onclick = fightDragon;
-    text.innerText = "You are in the town square. You see a sign that says Store.";
+    update(locations[0]);
 }
 
 function goStore() {
@@ -58,4 +74,14 @@ function buyHealth() {
 
 function buyWeapon() {
 
+}
+
+function update(location){
+    button1.innerText = location["button text"];
+    button2.innerText = "Go to cave";
+    button3.innerText = "Fight dragon";
+    button1.onclick = goStore;
+    button2.onclick = goCave;
+    button3.onclick = fightDragon;
+    text.innerText = "You are in the town square. You see a sign that says \"Store\".";
 }
