07ed30eb7e200a5ed4fd9c6ce61506fce20b7173
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-02-04 18:58:14 -0300

    132 to 135

--- Building_an_RPG/script.js
+++ Building_an_RPG/script.js
@@ -52,6 +52,12 @@ const locations = [
             "button text": ["Attack","Dodge","Run"],
             "button functions": [attack, dodge, goTown],
             text: "You are fighting a monster."
+        },
+        {
+            name: "kill monster",
+            "button text": ["Go to town square", "Go to town square", "Go to town square"],
+            "button functions": [goTown, goTown, goTown],
+            text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
         }
     ];
 const weapons = [
@@ -192,6 +198,7 @@ function lose(){
 }
 
 function update(location){
+    monsterStats.style.display = "none";
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
