8fd04f4fdaed2d8d980d72bf60517c5378aeb05a
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-02-04 00:22:49 -0300

    96 to 107

--- Building_an_RPG/script.js
+++ Building_an_RPG/script.js
@@ -108,7 +108,15 @@ function buyWeapon() {
 }
 
 function sellWeapon() {
+    if(inventory.length > 1){
+        gold += 15;
+        goldText.innerText = gold;
+        let currentWeapon = inventory.shift();
+        text.innerText = "You sold a " + currentWeapon + ".";
+        text.innerText += " In your inventory you have: " + inventory;
+    } else {
 
+    }
 }
 
 function fightSlime() {
