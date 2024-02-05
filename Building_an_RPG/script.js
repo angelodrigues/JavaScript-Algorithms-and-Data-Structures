e4a49ec86e7d1cdcf6678f46b34582c03a92e951
Angelo Barcelos <angelorodrigues318@gmail.com>
2024-02-04 23:12:48 -0300

    Finished

--- Building_an_RPG/script.js
+++ Building_an_RPG/script.js
@@ -83,7 +83,7 @@ const locations = [
             "button functions": [
                 goTown, 
                 goTown, 
-                goTown
+                easterEgg
             ],
             text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
         },
@@ -114,6 +114,20 @@ const locations = [
                 restart
             ],
             text: "You defeat the dragon! YOU WIN THE GAME! 🎉"
+        },
+        {
+            name: "easter egg",
+            "button text": [
+                "2", 
+                "8", 
+                "Go to town square?"
+            ],
+            "button functions": [
+                pickTwo, 
+                pickEight, 
+                goTown
+            ],
+            text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
         }
     ];
 const weapons = [
@@ -243,6 +257,10 @@ function attack() {
         defeatMonster();
       }    
     }
+    if (Math.random() <= .1 && inventory.length !== 1) {
+        text.innerText += " Your " + inventory.pop() + " breaks.";
+        currentWeapon--;
+    }
 }
 
 function dodge(){
@@ -295,5 +313,40 @@ function getMonsterAttackValue(level) {
 }
 
 function isMonsterHit(){
-    return Math.random() > .2;
+    return Math.random() > .2 || health < 20;
+}
+
+function easterEgg(){
+    update(locations[7]);
+}
+
+function pick(guess){
+    const numbers = [];
+    while(numbers.length < 10){
+        numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = "You picked " + guess + ". Here are the random numbers:\n";
+    for (let i = 0; i < 10; i++) {
+        text.innerText += numbers[i] + "\n";
+    }
+    if (numbers.includes(guess)) {
+        text.innerText += "Right! You win 20 gold!";
+        gold += 20;
+        goldText.innerText = gold;
+    } else {
+        text.innerText += "Wrong! You lose 10 health!";
+        health -= 10;
+        healthText.innerText = health;
+        if(health <= 0){
+            lose();
+        }
+    }
+}
+
+function pickTwo(){
+    pick(2);
+}
+  
+function pickEight(){
+    pick(8);
 }
